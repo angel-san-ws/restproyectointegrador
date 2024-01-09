5952f8e2aa62301f28b2759f9c8b88f4a8e9e3eb
Angel Sánchez <anghelo@gmail.com>
2024-01-09 08:40:47 -0600

    Initial commit

---
+++ src/main/java/com/angel/restproyectointegrador/RestproyectointegradorApplication.java
@@ -0,0 +1,13 @@
+package com.angel.restproyectointegrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestproyectointegradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestproyectointegradorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/angel/restproyectointegrador/RestproyectointegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angel.restproyectointegrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestproyectointegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
